9478dd2cb8866ad5df291fdd672f79da534d0cea
Varun <163327785+varunbudati@users.noreply.github.com>
2024-08-07 11:44:59 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,7 +175,7 @@ def main():
     
     # Portfolio icon (using a simple document icon as an example)
     portfolio_icon = """
-    <svg xmlns="https://varunbudati.github.io/assets/images/varun-budati.jpeg" viewBox="0 0 24 24">
+    <svg xmlns="https://varunbudati.github.io/assets/images/icon.ico" viewBox="0 0 24 24">
         <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 2l5 5h-5V4zM6 20V4h6v6h6v10H6z"/>
     </svg>
     """
